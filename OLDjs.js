d6d01611fda940bff57325fd6971b758a3487a50
ehsia-hsia <erica.hsia1@gmail.com>
2021-03-09 14:34:39 -0500

    Clear Button
    Clear all input values and toggle correct or incorrect styles

--- OLDjs.js
+++ OLDjs.js
@@ -199,3 +199,333 @@ function A4L3() {
 function A4L4() {
   return bees4 !== true ? incorrectColorChange(this) : correctColorChange(this);
 }
+
+// for (let i = 0; i < input.length; i++) {
+//   input[i].addEventListener("keyup", wordChecker);
+//   function wordChecker() {
+
+//       if (input.value == letter) {
+//         document.body.backgroundColor = "pink";
+//       }
+//     }
+//   }
+// }
+
+//_________LIST CHANGE______________
+
+// let scamCounter = 0;
+// let toneCounter = 0;
+// let artsCounter = 0;
+// let beesCounter = 0;
+
+// function wordChecker(input) {//   //Scam Check
+
+//   for (let y = 0; y < scam.length; y++) {
+//     if (input.value.match(scam[y][1])) {
+//       scamCounter++;
+//     }
+//   }
+//   if (scamCounter === 4) {
+//     listItem[0].style.color = "blue";
+//   }
+
+//   //Tone Check
+//   for (z = 0; z < tone.length; z++) {
+//     if (input.value == tone[z][1] && input == tone[z][0]) {
+//       toneCounter++;
+//     }
+//   }
+//   if (toneCounter == 4) {
+//     listItem[1].style.color = "blue";
+//   }
+//   //Arts Check
+//   for (a = 0; a < arts.length; a++) {
+//     if ((input.value = arts[a][1] && input == arts[a][0])) {
+//       artsCounter++;
+//     }
+//   }
+//   if (artsCounter == 4) {
+//     listItem[2].style.color = "red";
+//   }
+//   //Bees Check
+//   for (b = 0; b < bees.length; b++) {
+//     if (input.value == bees[b][1]) {
+//       beesCounter++;
+//     }
+//   }
+//   if (beesCounter === 4) {
+//     listItem[3].style.color = "blue";
+//   }
+// }
+
+// //--------------------------CORRECT WORDS-----------------------------//
+
+// let input = document.getElementsByTagName("input");
+// for (let i = 0; i < input.length; i++) {
+//   input[i].addEventListener("keyup", checkWord);
+// }
+
+// let scamCheck;
+// let toneCheck;
+// let artsCheck;
+// let beesCheck;
+
+// function checkWord() {
+//   if (
+//     A1_L1.value.toUpperCase().match("S") &&
+//     A1_L2.value.toUpperCase().match("C") &&
+//     A1_L3.value.toUpperCase().match("A") &&
+//     A1_L4.value.toUpperCase().match("M")
+//   ) {
+//     scamCheck = true;
+//   }
+//   if (
+//     A2_L1.value.toUpperCase().match("T") &&
+//     A2_L2.value.toUpperCase().match("O") &&
+//     A2_L3.value.toUpperCase().match("N") &&
+//     A2_L4.value.toUpperCase().match("E")
+//   ) {
+//     toneCheck = true;
+//   }
+//   if (
+//     A3_L1.value.toUpperCase().match("A") &&
+//     A3_L2.value.toUpperCase().match("R") &&
+//     A3_L3.value.toUpperCase().match("T") &&
+//     A3_L4.value.toUpperCase().match("S")
+//   ) {
+//     artsCheck = true;
+//   }
+//   if (
+//     A4_L1.value.toUpperCase().match("B") &&
+//     A4_L2.value.toUpperCase().match("E") &&
+//     A4_L3.value.toUpperCase().match("E") &&
+//     A4_L4.value.toUpperCase().match("S")
+//   ) {
+//     beesCheck = true;
+//   }
+//   if (
+//     scamCheck == true &&
+//     toneCheck == true &&
+//     artsCheck == true &&
+//     beesCheck == true
+//   ) {
+//     document.querySelector("body").style.backgroundColor = "green";
+//   }
+// }
+// //-------------LIST-----------
+// // LIST CHECK
+// for (let i = 0; i < input.length; i++) {
+//   input[i].addEventListener("keyup", list);
+// }
+// function correctListItem(listI) {
+//   listI.style.color = "blue";
+// }
+
+// function list() {
+//   if (scamCheck == true) {
+//     let item = document.getElementById("scamclue");
+//     correctListItem(item);
+//   }
+//   if (toneCheck == true) {
+//     let item = document.getElementById("toneclue");
+//     correctListItem(item);
+//   }
+//   if (artsCheck == true) {
+//     let item = document.getElementById("artsclue");
+//     correctListItem(item);
+//   }
+//   if (beesCheck == true) {
+//     let item = document.getElementById("beesclue");
+//     correctListItem(item);
+//   }
+// }
+
+// ///------------BUTTONS----------------------------------
+// //CLEAR ALL BUTTON
+// let clearButton = document.getElementById("clearAll");
+// clearButton.addEventListener("click", clearAll);
+
+// function clearAll() {
+//   document.querySelector("body").style.backgroundColor = "white";
+//   let input = document.getElementsByTagName("input");
+//   let listItem = document.getElementsByTagName("li");
+//   for (let j = 0; j < listItem.length; j++) {
+//     listItem[j].style.color = "black";
+//   }
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i].type == "text") {
+//       input[i].value = "";
+//       input[i].style.backgroundColor = "white";
+//     }
+//   }
+//   scam1 = false;
+//   scam2 = false;
+//   scam3 = false;
+//   scam4 = false;
+//   tone1 = false;
+//   tone2 = false;
+//   tone3 = false;
+//   tone4 = false;
+//   arts1 = false;
+//   arts2 = false;
+//   arts3 = false;
+//   arts4 = false;
+//   bees1 = false;
+//   bees2 = false;
+//   bees3 = false;
+//   bees4 = false;
+//   scamCheck = false;
+//   toneCheck = false;
+//   artsCheck = false;
+//   beesCheck = false;
+// }
+
+// //Reveal All
+// let revealButton = document.getElementById("revealAll");
+// revealButton.addEventListener("click", revealAll);
+
+// function blackText(aNum) {
+//   aNum.style.color = "black";
+// }
+
+// function revealAll() {
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i].type == "text") {
+//       input[i].style.color = "black";
+//     }
+//   }
+
+//   if (scam4 !== true) {
+//     A1_L4.value = "m";
+//     A1_L4.style.color = "black";
+//   }
+//   if (scam3 !== true) {
+//     A1_L3.value = "a";
+//     blackText(this);
+//   }
+//   if (scam2 !== true) {
+//     A1_L2.value = "c";
+//     blackText(this);
+//   }
+//   if (scam1 !== true) {
+//     A1_L1.value = "s";
+//     blackText(this);
+//   }
+//   if (A2_L4.value != "e") {
+//     A2_L4.value = "e";
+//     blackText(this);
+//   }
+//   if (A2_L3.value != "n") {
+//     A2_L3.value = "n";
+//     blackText(this);
+//   }
+//   if (A2_L2.value != "o") {
+//     A2_L2.value = "o";
+//     blackText(this);
+//   }
+//   if (A2_L1.value != "t") {
+//     A2_L1.value = "t";
+//     blackText(this);
+//   }
+//   if (A3_L1.value != "a") {
+//     A3_L1.value = "a";
+//     blackText(this);
+//   }
+//   if (A3_L2.value != "r") {
+//     A3_L2.value = "r";
+//     blackText(this);
+//   }
+//   if (A3_L3.value != "t") {
+//     A3_L3.value = "t";
+//     blackText(this);
+//   }
+//   if (A3_L4.value != "s") {
+//     A3_L4.value = "s";
+//     blackText(this);
+//   }
+//   if (A4_L4.value != "s") {
+//     A4_L4.value = "s";
+//     blackText(this);
+//   }
+//   if (A4_L3.value != "e") {
+//     A4_L3.value = "e";
+//     blackText(this);
+//   }
+//   if (A4_L2.value != "e") {
+//     A4_L2.value = "e";
+//     blackText(this);
+//   }
+//   if (A4_L1.value != "b") {
+//     A4_L1.value = "b";
+//     blackText(this);
+//   }
+// }
+
+//--------------SOME OG SAMPLES-------------------
+
+/* check list
+-congrats button
+-reveal button
+*/
+
+//CONGRATS BUTTON
+/*let congrats = document.getElementById("congrats");
+congrats.addEventListener("click", congratsFunction);
+
+function congratsFunction() {
+  if (
+    A1_L1.value.toUpperCase().match("S") &&
+    A1_L2.value.toUpperCase().match("C") &&
+    A1_L3.value.toUpperCase().match("A") &&
+    A1_L4.value.toUpperCase().match("M") &&
+    A2_L1.value.toUpperCase().match("T") &&
+    A2_L2.value.toUpperCase().match("O") &&
+    A2_L3.value.toUpperCase().match("N") &&
+    A2_L4.value.toUpperCase().match("E") &&
+    A3_L1.value.toUpperCase().match("A") &&
+    A3_L2.value.toUpperCase().match("R") &&
+    A3_L3.value.toUpperCase().match("T") &&
+    A3_L4.value.toUpperCase().match("S") &&
+    A4_L1.value.toUpperCase().match("B") &&
+    A4_L2.value.toUpperCase().match("E") &&
+    A4_L3.value.toUpperCase().match("E") &&
+    A4_L4.value.toUpperCase().match("S")
+  ) {
+    congrats.innerHTML = "congrats";
+    congrats.style.padding = "7%";
+  } else {
+    congrats.innerHTML = "you're dumb, check again";
+    congrats.style.textAlign = "center";
+    congrats.style.padding = "0";
+  }
+}
+*/
+
+// // 1 ACROSS: SCAM
+// let A1_L1 = document.getElementById("across1L1Input");
+// A1_L1.addEventListener("keyup", A1L1);
+
+// function A1L1() {
+//   A1_L1.style.color = "black";
+
+//   if (!A1_L1.value.toUpperCase().match("S")) {
+//     A1_L1.style.color = "red";
+//     A1_L1.style.backgroundColor = "white";
+//     A1_L1.addEventListener("click", clearletter);
+//     function clearletter() {
+//       this.value = "";
+//     }
+//   } else {
+//     A1_L1.style.color = "blue";
+//     A1_L1.style.backgroundColor = "lightblue";
+//   }
+// }
+// let A1_L2 = document.getElementById("across1L2Input");
+// A1_L2.addEventListener("keyup", A1L2);
+// function A1L2() {
+//   if (!A1_L2.value.toUpperCase().match("C")) {
+//     incorrectColorChange(this);
+//   } else {
+//     correctColorChange(this);
+//   }
+// }
