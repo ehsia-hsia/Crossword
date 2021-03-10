39d24749f1ceb0b7ea7728a7aed8e4cb9696596c
ehsia-hsia <erica.hsia1@gmail.com>
2021-03-10 10:15:15 -0500

    Modal on completion
    Modal opens when congrats counter greater than input length

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ let congratsCounter = 0;
 function congratsCounterStyles() {
   if (congratsCounter >= input.length) {
     status.textContent = "Status: COMPLETE!";
+    openModal();
   } else if (congratsCounter >= input.length * 0.6) {
     status.textContent = "Status: Almost complete...";
   } else if (congratsCounter >= input.length * 0.5) {
@@ -195,3 +196,47 @@ function clearAll() {
     }
   }
 }
+for (let i = 0; i < listItem.length; i++) {
+  listItem[i].addEventListener("click", clueHighlight);
+  function clueHighlight() {
+    if (listItem[i] == listItem[0]) {
+      input[0].style.borderColor = "pink";
+      input[1].style.borderColor = "pink";
+      input[2].style.borderColor = "pink";
+      input[3].style.borderColor = "pink";
+    }
+  }
+}
+
+("use strict");
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  ///creates object
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
